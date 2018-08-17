ddb6115c3257e7e1a3a9daa18dcfa103cf0382f1
Joao Grilo <joao.grilo@gmail.com>
2018-08-17 02:09:35 +0200

    Fix the filename, otherwise test won't find modules.

--- haircut/template/conftest.py
+++ haircut/template/conftest.py
@@ -9,4 +9,4 @@ class File(haircut.template.base.File):
 
     def __init__(self, *args, **kwargs):
         super(File, self).__init__(*args, **kwargs)
-        self.path = os.path.join(self.props["name"], 'confest.py')
+        self.path = os.path.join(self.props["name"], 'conftest.py')
